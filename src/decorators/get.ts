598d21658c478eb672442d4267185cc435e238d8
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-10-12 21:34:51 +0300

    refactor: move decorators folder under src/

---
+++ src/decorators/get.ts
@@ -0,0 +1,17 @@
+import { createPath } from "../utils";
+
+
+export default function Get(path?: string) {
+  return function (target: any, key: string) {
+    if (!target) return;
+
+    const routePath = path ? createPath(path) : createPath(key);
+    Reflect.defineMetadata("path", routePath, target, key);
+    if (!target[key]) return;
+
+    if (typeof target[key] !== "function")
+      throw new Error("Get decorator can only be used on functions");
+    // TODO: type check for (ctx: Context) => Response | Promise<Response>
+    Reflect.defineMetadata("method", "get", target, key);
+  };
+}
