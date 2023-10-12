c7c0dd24278905b3fb2fa56838e743f2bee7b9bb
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-10-12 21:35:19 +0300

    feat: add test.ts

---
+++ test.ts
@@ -0,0 +1,35 @@
+import { Config, Context, Serve } from ".";
+import Controller from "./src/decorators/controller";
+import Get from "./src/decorators/get";
+import Post from "./src/decorators/post";
+
+@Controller("/test")
+class TestController2 {
+  @Get("/test")
+  public async test1(ctx: any) {
+    return new Response("Hello World Get");
+  }
+
+  @Post("/test")
+  public async test2(ctx: any) {
+    return new Response("Hello World Post");
+  }
+
+  @Get()
+  public async test3() {
+    // TODO fix this
+    await new Promise<void>((resolve) => {
+      setTimeout(() => {
+        throw new Error(" test3");
+      }, 100);
+    });
+
+    return new Response("Hello World3");
+  }
+}
+
+const config: Config = {
+  controllers: [TestController2],
+};
+
+Serve(config);
