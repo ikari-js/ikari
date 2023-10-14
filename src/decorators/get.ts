65c358e7a1fdc954029fe5d8716bf6d8968a2ee3
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-10-15 00:27:40 +0300

    refactor: remove type check TODO

--- src/decorators/get.ts
+++ src/decorators/get.ts
@@ -1,17 +1,13 @@
 import { createPath } from "../utils";
 
-
 export default function Get(path?: string) {
   return function (target: any, key: string) {
     if (!target) return;
-
-    const routePath = path ? createPath(path) : createPath(key);
-    Reflect.defineMetadata("path", routePath, target, key);
     if (!target[key]) return;
-
     if (typeof target[key] !== "function")
       throw new Error("Get decorator can only be used on functions");
-    // TODO: type check for (ctx: Context) => Response | Promise<Response>
+    const routePath = path ? createPath(path) : createPath(key);
+    Reflect.defineMetadata("path", routePath, target, key);
     Reflect.defineMetadata("method", "get", target, key);
   };
 }
