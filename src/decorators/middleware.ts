0e86fe63c581bf3de2230d4099b3224f01cb4736
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-10-15 00:27:20 +0300

    feat: add TODO for middleware

--- src/decorators/middleware.ts
+++ src/decorators/middleware.ts
@@ -2,6 +2,7 @@ import { Handler }  from "../type";
 
 export default function Middleware(...middlewares: Handler[]) {
   return function (target: any, propertyKey: string | symbol) {
+    // TODO Befor or After
     Reflect.defineMetadata("preMiddlewares", middlewares, target, propertyKey);
   };
 }
