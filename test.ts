d2efe152c7e32ca7744359d6a2bd4cbf996e707c
Utku Erdemir <56741781+UtkuErdemir@users.noreply.github.com>
2023-10-15 22:57:41 +0300

    feat: updated test file code for async operations

--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@ import Controller from "./src/decorators/controller";
 import Get from "./src/decorators/get";
 import Middleware from "./src/decorators/middleware";
 import Post from "./src/decorators/post";
-import {  Serve } from ".";
+import { Serve } from ".";
 import { Context } from "./context";
 
 function testMiddleware(ctx: Context): Context {
@@ -16,7 +16,7 @@ function testMiddleware(ctx: Context): Context {
 class TestController2 {
   @Post("/test/:id/wqe/:id2")
   public async test1(ctx: Context) {
-    console.log("hit test1")
+    console.log("hit test1");
     console.log(ctx.param("id"));
     console.log(ctx.param("id2"));
   }
@@ -28,15 +28,19 @@ class TestController2 {
 
   @Get()
   public async test3() {
-    // TODO fix this
-    await new Promise<void>((resolve) => {
-      setTimeout(() => {
-        throw new Error(" test3");
-      }, 100);
-    });
+    await test();
 
     return new Response("Hello World3");
   }
+}
+
+async function timeout(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function test() {
+  await timeout(1000);
+  throw new Error("Test Error");
 }
 
 const config: Config = {
