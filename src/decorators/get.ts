ae6e1235a5173ac6cf53d7478b9f9d4f7699369d
Utku Erdemir <56741781+UtkuErdemir@users.noreply.github.com>
2023-10-16 23:39:46 +0300

    feat: added param and return type check

--- src/decorators/get.ts
+++ src/decorators/get.ts
@@ -1,3 +1,4 @@
+import { Context } from "../../context";
 import { createPath } from "../utils";
 
 export default function Get(path?: string) {
@@ -6,6 +7,28 @@ export default function Get(path?: string) {
     if (!target[key]) return;
     if (typeof target[key] !== "function")
       throw new Error("Get decorator can only be used on functions");
+    const paramTypes = Reflect.getMetadata("design:paramtypes", target, key);
+
+    if (paramTypes.length > 1)
+      throw new Error(
+        "Get decorator can only be used on functions with one parameter"
+      );
+    if (paramTypes.length === 1 && paramTypes[0] !== Context)
+      throw new Error(
+        "Get decorator can only be used on functions with one parameter of type Context"
+      );
+
+    const returnType = Reflect.getMetadata("design:returntype", target, key);
+
+    // We have to recheck PromiseResponse.
+    const validReturnTypes = [Context, Response, Promise<Response>];
+
+    if (returnType && !validReturnTypes.includes(returnType))
+      throw new Error(
+        "Get decorator can only be used on functions with return type of string"
+      );
+
+    console.log(Reflect.getMetadata("design:returntype", target, key));
     const routePath = path ? createPath(path) : createPath(key);
     Reflect.defineMetadata("path", routePath, target, key);
     Reflect.defineMetadata("method", "get", target, key);
