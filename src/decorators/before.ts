740f89afdd3743019d67fc18bdbf8b9f15117d9d
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-10-20 21:10:18 +0300

    feat: add before decorator

---
+++ src/decorators/before.ts
@@ -0,0 +1,20 @@
+import { Handler } from "../type";
+
+/**
+ * Before decorator, used to add middleware before a route handler. Middleware will be executed in order.
+ * @param handlers
+ */
+export function Before(...handlers: Handler[]) {
+  return function (target: any, key: string) {
+    if (!target) return;
+    if (!target[key]) return;
+    if (typeof target[key] !== "function")
+      throw new Error("Before decorator can only be used on functions");
+
+    const before = Reflect.getMetadata("before", target, key);
+    if (before) {
+      handlers = [...before, ...handlers];
+    }
+    Reflect.defineMetadata("before", handlers, target, key);
+  };
+}
