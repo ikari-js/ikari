0d1ba5702fb43d24d902cf8a198ea36e5ef141bf
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-10-23 20:34:46 +0300

    test: refactore test file

--- test.ts
+++ test.ts
@@ -1,51 +1,34 @@
 import { Config } from "./src/type";
 import { Serve } from ".";
 import { Context } from "./context";
-import { Before, Controller, Get, Post,After } from "./src/decorators";
-function testMiddlewareBefore(ctx: Context): void {
+import { Before, Controller, Get, Post, After } from "./src/decorators";
+
+function testMiddlewareBefore(ctx: Context, next: Next) {
   console.log("testMiddleware before");
+  next();
 }
 
-function testMiddlewareAfter(ctx: Context): Context {
+function testMiddlewareAfter(ctx: Context, next: Next) {
   console.log("testMiddleware after");
-  console.log(ctx.getResHeader("x-test"));
-
-  return ctx.status(200).json({ data: "Hello World After" });
+  ctx.status(200).json({ data: "Hello World After" });
 }
 
-@Controller("/test")
-class TestController2 {
-  @Post("/test/:id/wqe/:id2")
-  public async test1(ctx: Context) {
-    console.log("hit test1");
-    console.log(ctx.param("id"));
-    console.log(ctx.param("id2"));
-    const body = await ctx.body();
-    console.log(body.data);
-  }
+function testMiddlewareAfter2(ctx: Context, next: Next) {
+  console.log("testMiddleware after2");
+  ctx.status(200).json({ data: "Hello World After2" });
+}
 
-  @Get("/test2")
-  public async test2(ctx: Context): Promise<Response> {
-    return new Response("Hello World Post");
-  }
+type Next = () => void;
+
+@Controller("/")
+class TestController2 {
 
   @Before(testMiddlewareBefore)
-  @After(testMiddlewareAfter)
-  @Get("/redirect")
-  public test3(ctx: Context): Context {
-    ctx.set("x-test", "x-test");
-    console.log("hit test3");
-    return ctx.status(204).json({ data: "Hello World" });
+  @After(testMiddlewareAfter, testMiddlewareAfter2)
+  @Get("")
+  public test(ctx: Context, next: Next): Context {
+    return ctx.json({ data: "Hello World" });
   }
-}
-
-async function timeout(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function test() {
-  await timeout(1000);
-  throw new Error("Test Error");
 }
 
 const config: Config = {
