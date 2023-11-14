225885df67134c0c8d48d9adc54eef281a90230c
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-11-14 22:10:38 +0300

    feat: add head method

---
+++ src/decorators/head.ts
@@ -0,0 +1,25 @@
+import { Context } from "../../context";
+import { Handler } from "../type";
+import { createPath } from "../utils";
+
+/**
+ * Decorator for defining a route that handles GET requests
+ *
+ * @param path - The path to the route (optional). By default it will use the name of the function.
+ *
+ **/
+export function Head(path?: string) {
+  if (path && typeof path !== "string")
+    throw new Error("Path must be a string");
+
+  return function (target: any, key: string) {
+    if (!target) return;
+    if (!target[key]) return;
+    if (typeof target[key] !== "function")
+      throw new Error("Head decorator can only be used on functions");
+
+    const routePath = path ? createPath(path) : createPath(key);
+    Reflect.defineMetadata("path", routePath, target, key);
+    Reflect.defineMetadata("method", "head", target, key);
+  };
+}
--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -4,3 +4,4 @@ export * from "../decorators/controller";
 export * from "../decorators/get";
 export * from "../decorators/middleware";
 export * from "../decorators/post";
+export * from "../decorators/head";
