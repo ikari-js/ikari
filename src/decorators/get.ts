0f6a9d56d11d56286d5e7bb7b17d10d865c02a35
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-11-14 19:34:22 +0300

    refactor: remove type check

--- src/decorators/get.ts
+++ src/decorators/get.ts
@@ -18,26 +18,6 @@ export function Get(path?: string) {
     if (!target[key]) return;
     if (typeof target[key] !== "function")
       throw new Error("Get decorator can only be used on functions");
-    const paramTypes = Reflect.getMetadata("design:paramtypes", target, key);
-    // TODO fix this now we have context and next in the params
-    if (paramTypes.length > 2)
-      throw new Error(
-        "Get decorator can only be used on functions with one parameter"
-      );
-    if (paramTypes.length === 1 && paramTypes[0] !== Context)
-      throw new Error(
-        "Get decorator can only be used on functions with one parameter of type Context"
-      );
-
-    const returnType = Reflect.getMetadata("design:returntype", target, key);
-
-    const validReturnTypes = [Context, Response, Promise];
-
-    if (returnType && !validReturnTypes.includes(returnType))
-      throw new Error(
-        "Get decorator can only be used on functions with return type of string"
-      );
-
     const routePath = path ? createPath(path) : createPath(key);
     Reflect.defineMetadata("path", routePath, target, key);
     Reflect.defineMetadata("method", "get", target, key);
