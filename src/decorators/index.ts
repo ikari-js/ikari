8915719f8da3025b93134aa7d7b62666dd572c11
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-11-16 22:39:28 +0300

    refactor: remove middleware

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -2,6 +2,5 @@ export * from "../decorators/before";
 export * from "../decorators/after";
 export * from "../decorators/controller";
 export * from "../decorators/get";
-export * from "../decorators/middleware";
 export * from "../decorators/post";
 export * from "../decorators/head";
