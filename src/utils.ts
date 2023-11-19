a31b61d387970f4de9dba3ff24193afbe2f55211
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-11-19 23:18:03 +0300

    refactor: move HttpMethod to utils

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,21 @@ export function createPath(str: string) {
   }
   return str;
 }
+export const representationHeaders = [
+  "Content-Type",
+  "Content-Encoding",
+  "Content-Language",
+  "Content-Location",
+];
+
+export enum HttpMethod {
+  GET = "get",
+  POST = "post",
+  PUT = "put",
+  DELETE = "delete",
+  PATCH = "patch",
+  OPTIONS = "options",
+  HEAD = "head",
+  ALL = "all",
+}
+
