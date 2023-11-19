8b22574e4f5a726c8d88930a4649ae060724e2ce
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-11-19 23:17:23 +0300

    feat: add all, delete, options, patch and put

---
+++ src/decorators/put.ts
@@ -0,0 +1,24 @@
+import { createPath } from "../utils";
+
+/**
+ * Decorator for defining a route that handles PUT requests
+ *
+ * @param path - The path to the route (optional). By default it will use the name of the function.
+ *
+ **/
+export function Put(path?: string) {
+  if (path && typeof path !== "string")
+    throw new Error("Path must be a string");
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return function (target: any, key: string) {
+    if (!target) return;
+    if (!target[key]) return;
+    if (typeof target[key] !== "function")
+      throw new Error("Put decorator can only be used on functions");
+
+    const routePath = path ? createPath(path) : createPath(key);
+    Reflect.defineMetadata("path", routePath, target, key);
+    Reflect.defineMetadata("method", "put", target, key);
+  };
+}
