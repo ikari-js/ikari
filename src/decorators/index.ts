c32f2e9ec67ad225b9864eb1168624c8216e3e50
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-11-24 22:06:18 +0300

    feat: add export types

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -4,3 +4,8 @@ export * from "../decorators/controller";
 export * from "../decorators/get";
 export * from "../decorators/post";
 export * from "../decorators/head";
+export * from "../decorators/put";
+export * from "../decorators/patch";
+export * from "../decorators/delete";
+export * from "../decorators/options";
+
