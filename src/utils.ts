f8cf59df55e85c8bad171d451d3e413182a71a22
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-11-26 17:44:53 +0300

    feat: add httpStatus codes

--- src/utils.ts
+++ src/utils.ts
@@ -27,6 +27,40 @@ export enum HttpMethod {
   ALL = "all",
 }
 
+export enum StatusCode {
+    OK = 200,
+    CREATED = 201,
+    ACCEPTED = 202,
+    NO_CONTENT = 204,
+    MOVED_PERMANENTLY = 301,
+    FOUND = 302,
+    SEE_OTHER = 303,
+    NOT_MODIFIED = 304,
+    TEMPORARY_REDIRECT = 307,
+    PERMANENT_REDIRECT = 308,
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    PAYMENT_REQUIRED = 402,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404,
+    METHOD_NOT_ALLOWED = 405,
+    NOT_ACCEPTABLE = 406,
+    REQUEST_TIMEOUT = 408,
+    CONFLICT = 409,
+    GONE = 410,
+    LENGTH_REQUIRED = 411,
+    PAYLOAD_TOO_LARGE = 413,
+    URI_TOO_LONG = 414,
+    UNSUPPORTED_MEDIA_TYPE = 415,
+    EXPECTATION_FAILED = 417,
+    IM_A_TEAPOT = 418,
+    TOO_MANY_REQUESTS = 429,
+    INTERNAL_SERVER_ERROR = 500,
+    NOT_IMPLEMENTED = 501,
+    BAD_GATEWAY = 502,
+    SERVICE_UNAVAILABLE = 503,
+}
+
 export const resetColor = "\x1b[0m";
 export const greenColor = "\x1b[32m";
 export const blueColor = "\x1b[34m";
