df66dd3c29ae65d2abfe3c07f6538a832a0f16a8
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2023-12-02 20:58:55 +0300

    feat: add build script

---
+++ build.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+import { execSync } from "node:child_process";
+import { readdirSync } from "node:fs";
+try {
+  execSync("tsc -p tsconfig.build.json");
+
+  const files = readdirSync("./dist")
+    .filter((file) => file.endsWith(".js"))
+    .map((file) => `./dist/${file}`);
+
+  await Bun.build({
+    entrypoints: files,
+    target: "bun",
+    outdir: "./dist",
+    minify: true,
+  });
+
+  const decoratorsFiles = readdirSync("./dist/decorators")
+    .filter((file) => file.endsWith(".js"))
+    .map((file) => `./dist/decorators/${file}`);
+
+  await Bun.build({
+    entrypoints: decoratorsFiles,
+    target: "bun",
+    outdir: "./",
+    minify: true,
+  });
+} catch (e) {
+  console.error(e);
+}
