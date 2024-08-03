3e97bd4d4e3674b38fcaeb73b0a4cbf779705261
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2024-08-03 13:21:02 +0300

    chore: update build script

--- build.ts
+++ build.ts
@@ -1,24 +1,24 @@
-import { execSync } from "node:child_process";
+import { $ } from "bun";
 import { readdirSync } from "node:fs";
 
 try {
-  execSync("rm -rf dist");
-  execSync("tsc -p tsconfig.build.json");
+  await $`rm -rf ./dist`;
+  await $`tsc -p tsconfig.build.json`;
 } catch (error) {
   // eslint-disable-next-line no-console
   console.error(error);
 }
 
-const files = readdirSync("./dist", { recursive: true })
+const files = readdirSync("./src", { recursive: true })
   .filter((file) => typeof file === "string")
-  .filter((file) => (file as string).endsWith(".js"))
-  .map((file) => `./dist/${file}`);
+  .filter((file) => (file as string).endsWith(".ts"))
+  .map((file) => `./src/${file}`);
 
 const output = await Bun.build({
   entrypoints: files,
   target: "bun",
   outdir: "./dist",
-  root: "./dist",
+  root: "./src",
   minify: true,
 });
 
