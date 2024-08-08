edbd2bd060d8e9915c3ea8ac72bf38d834db3a46
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2024-08-08 20:51:14 +0300

    feat: router optimization

--- src/route.ts
+++ src/route.ts
@@ -1,26 +1,12 @@
 import { Context } from "./context";
-import { Handler, Handlers } from "./types";
-
-type RouteHandler = {
-  handler: Handler;
-  done: boolean;
-};
-
+import { Handlers } from "./types";
 export class Routes {
-  private routes: RouteHandler[] = [];
-  constructor(handlers: Handlers) {
-    this.routes = handlers.map((handler) => ({
-      handler,
-      // TODO find a better way to do this
-      done: false,
-    }));
-  }
+  constructor(private routes: Handlers) {}
 
-  public async next(ctx: Context): Promise<void | Context> {
+  public async next(ctx: Context) {
     // TODO: performance check
-    const nextHandler = this.routes.find((handler) => !handler.done);
+    const nextHandler = this.routes.shift();
     if (!nextHandler) return ctx;
-    nextHandler.done = true;
-    return nextHandler.handler(ctx);
+    return nextHandler(ctx);
   }
 }
