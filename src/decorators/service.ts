675d162cfe3d1581a119e72cfad656db469dbd34
Abdurrahman Aydın <abdurrahman262@hotmail.com>
2024-08-11 01:09:38 +0300

    refactor: correct type imports

--- src/decorators/service.ts
+++ src/decorators/service.ts
@@ -1,13 +1,13 @@
 import { Service as TypediService } from "typedi";
 import { Constructor } from "../types";
-import { ServiceOptions } from "typedi/types/interfaces/service-options.interface";
+import type { ServiceOptions } from "typedi";
 
 /**
  * Service decorator is used to define a service class. Marking a class with this decorator will make it a service. Services can be used in Controllers and other services without the need to instantiate them.
  * It should be used on a class.
- * 
+ *
  * @param {ServiceOptions} options - the options for the service decorator
- * 
+ *
  **/
 export function Service(options?: ServiceOptions) {
   return function <T>(target: Constructor<T>) {
